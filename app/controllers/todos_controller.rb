08e74c0d8b070ba159381211d08c4f72d699078c
Taga Yasunori <yasukun.taga@gmail.com>
2016-12-06 16:23:23 +0800

    scaffold todo

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,49 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+  # GET /todos
+  def index
+    @todos = Todo.all
+
+    render json: @todos
+  end
+
+  # GET /todos/1
+  def show
+    render json: @todo
+  end
+
+  # POST /todos
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      render json: @todo, status: :created, location: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /todos/1
+  def update
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /todos/1
+  def destroy
+    @todo.destroy
+  end
+
+  private
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+
+    def todo_params
+      params.permit(:title, :description)
+    end
+end
