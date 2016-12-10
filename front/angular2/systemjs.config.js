12e196c4a216a1ba4bafa57e34796fb85992003d
Taga Yasunori <yasukun.taga@gmail.com>
2016-12-11 00:36:55 +0800

    init angular2

--- front/angular2/systemjs.config.js
+++ front/angular2/systemjs.config.js
@@ -12,6 +12,7 @@
     map: {
       // our app is within the app folder
       app: 'app',
+
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -21,7 +22,7 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
+
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
