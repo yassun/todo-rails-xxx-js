290c5db112ebc1e2cf4e268b18985a6f373b45e6
Taga Yasunori <yasukun.taga@gmail.com>
2016-12-15 23:00:24 +0800

    fix routing

--- front/angular2/app/app-routing.module.ts
+++ front/angular2/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoComponent }        from './todo.component';
+import { TodoListComponent }    from './todo-list.component';
 const routes: Routes = [
+  { path: '',  component: TodoListComponent },
   { path: 'todo/:id', component: TodoComponent }
 ];
 @NgModule({
--- front/angular2/app/app.component.ts
+++ front/angular2/app/app.component.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'todo-app',
-  template: `
-    <h1>Todo App</h1>
-    <h2>Rails5 + Angular2 + TypeScript</h2>
-    <todo-list></todo-list>
-  `
+  template: `<router-outlet></router-outlet>`
 })
-export class AppComponent  {
-}
+export class AppComponent  { }
