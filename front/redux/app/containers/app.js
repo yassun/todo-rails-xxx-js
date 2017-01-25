121f43ec7b12063c0aeb6f0605a4009e1d505bb1
Taga Yasunori <yasukun.taga@gmail.com>
2017-01-25 22:38:06 +0900

    [add] containers

---
+++ front/redux/app/containers/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Todos from '../components/todos'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Todos></Todos>
+      </div>
+    );
+  }
+}
