a68f808fee0e45ba52c46d74879834b31328720b
Taga Yasunori <yasukun.taga@gmail.com>
2017-01-31 23:07:09 +0900

    [add] edit page

--- front/redux/app/routes/index.js
+++ front/redux/app/routes/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from '../containers/App';
+import Edit from '../containers/Edit';
 
 const routes = (
   <Route path="/" component={App} >
-    <Route path="edit" component={App}/>
+    <Route path="edit" component={Edit}/>
     <Route path="new" component={App} />
     <Route path="*" component={App} />
   </Route>
