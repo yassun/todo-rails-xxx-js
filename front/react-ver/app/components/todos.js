53a4324e25d249cb58ccda281fa9ff20ab9af99f
Taga Yasunori <yasukun.taga@gmail.com>
2017-02-16 22:52:53 +0900

    [add] todo link

--- front/react-ver/app/components/todos.js
+++ front/react-ver/app/components/todos.js
@@ -1,29 +1,20 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Todo from './todo'
+import { Link } from 'react-router'
 
 export default class Todos extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {todos: []}
-  }
-  componentDidMount() {
-    axios.get('/todos').then((response) => {
-      this.setState({todos: response.data})
-    }).catch((response) => {
-      console.log(response)
-    })
-  }
   render() {
     return (
       <ul>
-        {this.state.todos.map(todo =>
-          <Todo
-            key = {todo.id}
-            title = { todo.title }
-            description = { todo.description }
-           />
-        )}
+          {this.props.todos.map(todo =>
+            <Link key = { todo.id } to={`/todos/${todo.id}`}>
+              <Todo
+                title = { todo.title }
+                description = { todo.description }
+               />
+            </Link>
+          )}
       </ul>
     );
   }
