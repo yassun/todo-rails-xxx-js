4726fc04327ef9efa7433d1dac785147f63f348f
Taga Yasunori <yasukun.taga@gmail.com>
2017-02-19 18:24:22 +0900

    [add] css modules

--- front/react-ver/webpack.config.js
+++ front/react-ver/webpack.config.js
@@ -7,14 +7,33 @@ module.exports = {
     filename: "bundle.js"
   },
   module: {
-    loaders: [{
-      test: /\.css$/,
-      loader: "style!css"
-    }, {
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-     }]
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader?sourceMap',
+          'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+          'resolve-url',
+          'sass'
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-react-jsx'],
+        }
+      },
+    ]
   },
   resolve: {
     extensions: ['*', '.js', '.jsx', '.css']
