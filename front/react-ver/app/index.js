4872272699cf011567b5656408c7ac38fd89c39b
Taga Yasunori <yasukun.taga@gmail.com>
2017-02-21 21:57:24 +0900

    [add] app components

--- front/react-ver/app/index.js
+++ front/react-ver/app/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
+import App from "./components/app";
 import IndexPage from './pages/index'
 import TodoPage  from './pages/edit'
 import NewPage   from './pages/new'
 
 render((
   <Router history={hashHistory}>
-    <Route path="/" >
+    <Route path="/" component={App} >
       <IndexRoute component={IndexPage}/>
       <Route path="/todos/new" component={NewPage}/>
       <Route path="/todos/:id" component={TodoPage}/>
