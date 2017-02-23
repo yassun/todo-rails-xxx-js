30e49e44b3ad8f56ded448f4dfb3eacb64d1cac1
Taga Yasunori <yasukun.taga@gmail.com>
2017-02-23 21:44:47 +0900

    [fix] add CSS modules to todo component

--- front/react-ver/app/components/todo.js
+++ front/react-ver/app/components/todo.js
@@ -1,12 +1,40 @@
 import React, { Component ,PropTypes } from 'react'
+import { Link } from 'react-router'
+import styles from '../css/todo.css'
 
 export default class Todo extends Component {
   render() {
+    var todoClassNames = [
+      'col','s12','m6','l3'
+    ].join(' ');
+
+    var cardClassNames = [
+      styles.todo,
+      'card',
+      'hoverable'
+    ].join(' ');
+
+    var titleClassNames = [
+      styles.todoTitle,
+      'black-text'
+    ].join(' ');
+
+    var descriptionClassNames = [
+        styles.todoDescription, 
+        'grey-text'
+    ].join(' ');
+
     return (
-      <li>
-        <span>{this.props.title}</span><br/>
-        <span>{this.props.description}</span><br/>
-      </li>
+      <Link to={`/todos/${this.props.todo.id}`}>
+        <div className={todoClassNames}>
+          <div className={cardClassNames}>
+            <div className="card-content">
+              <span className={titleClassNames}>{this.props.todo.title}</span>
+              <p className={descriptionClassNames}>{this.props.todo.description}</p>
+            </div>
+          </div>
+        </div>
+      </Link>
     );
   }
 }
